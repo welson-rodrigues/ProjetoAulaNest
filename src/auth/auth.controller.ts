9db0d2dd23b3df70c58bd81e384f3e46639cf8a9
Zee <zee@debian.ifrn.local>
2025-01-23 14:29:34 -0300

    Mudanças na pasta auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,31 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
-import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Post,
+    Request,
+    UseGuards
+  } from '@nestjs/common';
+  import { AuthGuard } from './auth.guard';
+  import { AuthService } from './auth.service';
+  
+  @Controller('auth')
+  export class AuthController {
     constructor(private authService: AuthService) {}
-
+  
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signin(@Body() signinDto: Record<string, any>) {
-        return this.authService.signIn(signinDto.username, signinDto.password);
+    signIn(@Body() signInDto: Record<string, any>) {
+      return this.authService.signIn(signInDto.username, signInDto.password);
     }
-}
+  
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+      return req.user;
+    }
+  }
+  
