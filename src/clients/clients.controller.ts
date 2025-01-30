b3204d5e25b709e35b83cb0cb4b1072fdf039938
Zee <zee@debian.ifrn.local>
2025-01-30 14:18:13 -0300

    Criptografia da senha

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -2,13 +2,27 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+const crypto = require('crypto');
+import { jwtConstants } from '../auth/constants';
 
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
+  private encryptPassword(password: string): string{
+    const algoritm = 'aes-256-ctr';
+    const secretkey = process.env.SECRET_KEY || jwtConstants.secret;
+    const iv = crypto.randomBytes(16);
+
+    const cipher = crypto.createCipheriv(algoritm, Buffer.from(secretkey, 'utf-8'), iv)
+    let encryptPassword = cipher.update(password,'utf8', 'hex');
+    encryptPassword += cipher.final('hex');
+
+    return iv.toString('hex') + ':' + encryptPassword;
+  }
 
   @Post()
   create(@Body() createClientDto: CreateClientDto) {
+    const encryptPassword = this.encryptPassword(createClientDto.password)
     return this.clientsService.create(createClientDto);
   }
 
